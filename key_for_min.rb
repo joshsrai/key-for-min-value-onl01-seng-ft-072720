f1978b10c80dd475bdafc39f1d5f4b925e094ac7
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 18:12:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,3 +4,16 @@
 def key_for_min_value(name_hash)
 
 end
+
+
+birthday_kids = {
+    "Timmy" => 9, 
+    "Sarah" => 6, 
+    "Amanda" => 27
+}
+
+birthday_kids.collect do |kids_name, age|  
+    if age <= 12
+      puts "#{kids_name} #{age}"
+end
+end
