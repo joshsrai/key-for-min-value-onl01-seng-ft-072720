3d4088e738668dc36d34f9c4f86ffeb07cf10fe9
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 18:28:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,11 @@
 
 def key_for_min_value(name_hash)
    return nil if name_hash.size == 0
-   #get the first key:value pair by using .first method
-   min_key, min_value = name_hash.first
+   mkey, mvalue = name_hash.first
    name_hash.each do |key, value|
-      # iterate over each value to check which one smaller than the return value
-      if value < min_value
-      min_key = key #just need the key
+      if value < mvalue
+      mkey = key
       end
    end
-   min_key
+   mkey
 end
