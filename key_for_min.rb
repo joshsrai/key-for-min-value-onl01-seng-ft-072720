5767d3126b3847a07b6ab576ae53f979b3cb7656
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 18:21:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collection { |key, value|
   
-
-
-
+  win = ""
+  name_hash.collection { |key, value|
+  if value 
+  
+  
 end
-
-
-
-
-
 
 
 # passengers = {
